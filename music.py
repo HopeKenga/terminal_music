3cebc5163947c6877dee9acf0901d284a77acdfe
Hope Kenga <kengahope7@gmail.com>
2023-04-15 18:48:34 +0300

    feat: play a song

--- music.py
+++ music.py
@@ -1,6 +1,8 @@
+import os
 import spotipy
 import spotipy.util as util
-from dotenv import as load_dotenv
+from dotenv import load_dotenv
+
 
 # Load the environment file
 load_dotenv()
@@ -12,6 +14,14 @@ client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
 
 scope='user-read-private user-read-playback-state user-modify-playback-state'
-token=util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+token = util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+sp = spotipy.Spotify(auth=token)
 
+#play a song
+def play_track(track_name):
+    results = sp.search(q='track:' + track_name, type='track')
+    if results['tracks'] ['items'] :
+        tracks_uri = results['tracks']['items'][0]['uri']
+        sp.start_playback(uris=[tracks_uri])
+        
 
